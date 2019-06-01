1ff1ffda244d33f3d7d62dd687134c892e253336
ltoddy <taoliu0509@gmail.com>
2019-06-01 21:14:12 +0800

    config static path

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ const app: Application = express();
 app.set("port", PORT);
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 }));
 
 app.use("/", homeController);
 
