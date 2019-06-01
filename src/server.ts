53f00fb0aa9285a45902cc47250f4a99d0e70c0a
ltoddy <taoliu0509@gmail.com>
2019-06-01 21:45:37 +0800

    config logger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import app from "./app";
+import loggerFactory from "./utils/logger";
+
+const logger = loggerFactory("server.ts");
 
 const server = app.listen(app.get("port"), () => {
-    console.log(
-      "  App is running at http://localhost:%d in %s mode",
-      app.get("port"),
-      app.get("env")
-    );
+    logger.info(`  App is running at http://localhost:${app.get("port")} in ${app.get("env")} mode`);
     console.log("  Press CTRL-C to stop\n");
   }
 );
---
+++ src/utils/logger.ts
@@ -0,0 +1,20 @@
+import { createLogger, format, Logger, transports } from "winston";
+
+const { combine, timestamp, printf } = format;
+
+export default function loggerFactory(filename: string): Logger {
+  const formatter = printf(({ level, message, _, timestamp }) => {
+    return `${timestamp} [${filename}] ${level}: ${message}`;
+  });
+
+  return createLogger({
+    transports: [
+      new (transports.Console)({ level: process.env.NODE_ENV === "production" ? "error" : "debug" }),
+      new (transports.File)({ filename: "blog.log", level: "debug" })
+    ],
+    format: combine(
+      timestamp(),
+      formatter,
+    )
+  });
+}
