2be8edd8aaa56f2cfe6fd5ae4efe4e1bec7cd479
ltoddy <taoliu0509@gmail.com>
2019-06-11 22:19:49 +0800

    sign in view

---
+++ public/js/_flash.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  const closeBtn = document.getElementById("close");
+
+  if (closeBtn) {
+    closeBtn.onclick = function (event) {
+      event.preventDefault();
+      const navs = document.getElementsByClassName("flash-content");
+
+      for (let i = 0; i < navs.length; i++) {
+        navs[i].style.display = "none";
+      }
+    };
+  }
+};
