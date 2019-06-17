aba6750c478859b43ba23e1e2ac7bdaee247669c
ltoddy <taoliu0509@gmail.com>
2019-06-17 20:21:39 +0800

    fix

--- tests/routes/home.test.ts
+++ tests/routes/home.test.ts
@@ -2,10 +2,10 @@ import request = require("supertest");
 
 import app from "../../src/app";
 
-describe("GET /posts", () => {
+describe("GET /", () => {
   it("should return 200 OK", () => {
     return request(app)
-      .get("/posts")
+      .get("/")
       .expect(200);
   });
 });
