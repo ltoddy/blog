2af186ead69226b4ade1d3331d6f2d54ed813fc9
ltoddy <taoliu0509@gmail.com>
2019-06-17 09:23:22 +0800

    更新404页面

--- src/errors/404.ts
+++ src/errors/404.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 function pageNotFound(req: Request, res: Response) {
   if (!res.headersSent) {
-    res.status(404).render("404");
+    res.status(404).render("404", { post: undefined });
   }
 }
 
